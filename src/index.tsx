364b0787b15f76df119440d2861c100bab1bc8b8
Alexander Nicholson <alexn400@gmail.com>
2021-02-21 11:29:22 +1300

    Add bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
+import './style/main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
