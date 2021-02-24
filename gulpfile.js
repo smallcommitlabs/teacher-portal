dacfcf1f6c6549e1a63a3fd36f980bd53c5f8f19
Alexander Nicholson <alexn400@gmail.com>
2021-02-24 17:11:20 +1300

    Fix warnings

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ const bump = require('gulp-bump');
 const octo = require('@octopusdeploy/gulp-octo');
 const gulp = require('gulp');
 
-gulp.task('bump', function () {
-  return gulp
+gulp.task('bump', () =>
+  gulp
     .src('./package.json')
     .pipe(bump({ type: 'patch' }))
-    .pipe(gulp.dest('./'));
-});
+    .pipe(gulp.dest('./'))
+);
 
-gulp.task('publish', function () {
-  return gulp
+gulp.task('publish', () =>
+  gulp
     .src(['build/*'])
     .pipe(octo.pack())
-    .pipe(octo.push({ apiKey: process.env.octopusKey, host: 'https://linguaben.octopus.app' }));
-});
+    .pipe(octo.push({ apiKey: process.env.octopusKey, host: 'https://linguaben.octopus.app' }))
+);
